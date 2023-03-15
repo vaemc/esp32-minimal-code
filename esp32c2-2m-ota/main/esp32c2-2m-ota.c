1e3d7d293e70763947adaa0f8694f7a26eb4a521
麦草 <vaemc520@qq.com>
2023-03-15 13:05:24 +0800

    Initial commit

---
+++ esp32c2-2m-ota/main/esp32c2-2m-ota.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <esp_log.h>
+#include <string.h>
+#include "esp_event.h"
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_ota_ops.h"
+#include "esp_http_client.h"
+#include "esp_https_ota.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include "protocol_examples_common.h"
+
+static const char *TAG = "esp32-minimal-code";
+
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    switch (evt->event_id)
+    {
+    case HTTP_EVENT_ERROR:
+        ESP_LOGD(TAG, "HTTP_EVENT_ERROR");
+        break;
+    case HTTP_EVENT_ON_CONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_CONNECTED");
+        break;
+    case HTTP_EVENT_HEADER_SENT:
+        ESP_LOGD(TAG, "HTTP_EVENT_HEADER_SENT");
+        break;
+    case HTTP_EVENT_ON_HEADER:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+        break;
+    case HTTP_EVENT_ON_DATA:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+        break;
+    case HTTP_EVENT_ON_FINISH:
+        ESP_LOGD(TAG, "HTTP_EVENT_ON_FINISH");
+        break;
+    case HTTP_EVENT_DISCONNECTED:
+        ESP_LOGD(TAG, "HTTP_EVENT_DISCONNECTED");
+        break;
+    case HTTP_EVENT_REDIRECT:
+        ESP_LOGD(TAG, "HTTP_EVENT_REDIRECT");
+        break;
+    }
+    return ESP_OK;
+}
+
+void ota_task(void *pvParameter)
+{
+    esp_http_client_config_t config = {
+        .url = "http://vaemc-file.oss-cn-guangzhou.aliyuncs.com/esp32c2-2m-ota.bin?OSSAccessKeyId=LTAIq3AJYHaWHZaq&Expires=1678860135&Signature=MTuEdB%2FLH9q6sBDvOKeL6RfPw64%3D",
+        .event_handler = _http_event_handler,
+        .keep_alive_enable = true};
+
+    esp_https_ota_config_t ota_config = {
+        .http_config = &config,
+    };
+
+    ESP_LOGI(TAG, "Attempting to download update from %s", config.url);
+
+    esp_err_t ret = esp_https_ota(&ota_config);
+    if (ret == ESP_OK)
+    {
+        ESP_LOGI(TAG, "***********************************************");
+        ESP_LOGI(TAG, "OTA Succeed, Rebooting...");
+        ESP_LOGI(TAG, "*********************************************** \n");
+        esp_restart();
+    }
+    else
+    {
+        ESP_LOGI(TAG, "***********************************************");
+        ESP_LOGE(TAG, "Firmware upgrade failed");
+        ESP_LOGI(TAG, "*********************************************** \n");
+    }
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
+static void printf_task(void *pvParameters)
+{
+    while (1)
+    {
+        printf("Hello World \n");
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
+
+void app_main(void)
+{
+
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(example_connect());
+
+    xTaskCreate(&printf_task, "printf_task", 8192, NULL, 5, NULL);
+    xTaskCreate(&ota_task, "ota_task", 8192, NULL, 5, NULL);
+}
